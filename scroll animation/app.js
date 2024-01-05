b3fce9479654870ebb9085ea772dd0e8fb342bf7
Sai Ganesh Aavula <153426352+Gannuunix@users.noreply.github.com>
2024-01-05 12:13:29 +0530

    Add files via upload

---
+++ scroll animation/app.js
@@ -0,0 +1,12 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray('.section').forEach(section => {
+  ScrollTrigger.create({
+    trigger: section,
+    start: 'top top',
+    duration: 5,
+    pin: true,
+  
+    pinSpacing: false
+  });
+});
